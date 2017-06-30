f53f7bda687388dc90ad438d42c252105c616aab
Robin Shields-Cutler <cutler.robin@gmail.com>
2017-06-30 11:39:08 -0500

    Formatted references to work with markerGMG's tree.

--- analysis_code/como_zoo_09_microbiome.R
+++ analysis_code/como_zoo_09_microbiome.R
@@ -57,7 +57,7 @@ write.table(otus, file = "data/stitched/como_stitched_otu_filtered_final.txt",se
 
 
 # Do the same as above for the stitched taxa table
-taxa <- read.delim("../data/shi7_170628/como_stitched_taxa.txt",header = 1,row.names = 1,check.names = FALSE)
+taxa <- read.delim("**taxatablefile**",header = 1,row.names = 1,check.names = FALSE)
 szscids= c("SZ.SC1.S137.L001.",'SZ.SC2.S138.L001.',"SZ.SC3.S139.L001.","SZ.SC7.S143.L001.","SZ.SC4.S140.L001.","SZ.SC6.S142.L001.","SZ.SC5.S141.L001.")
 taxa <- taxa[, !(names(taxa) %in% szscids)]
 samples <- colnames(taxa)
@@ -82,5 +82,24 @@ taxa <- t(taxa)
 taxa <- data.frame(taxa)
 taxa <- tibble::rownames_to_column(taxa, var = "#OTU ID")
 write.table(taxa, file = "data/stitched/como_stitched_taxa_filtered_final.txt",sep = "\t", quote = FALSE, row.names = FALSE)
+
+# Format the references to match the tree for Refseq from markerGMG.
+otus <- read.delim('data/stitched/como_stitched_otu_filtered_final.txt', header=1, row.names = 1, check.names = F, sep = '\t')
+refs <- rownames(otus)
+refs <- lapply(refs, function(x) paste(strsplit(x, split = ' ')[[1]][1]))
+refs <- lapply(refs, function(x) paste(strsplit(x, split = '_')[[1]][1:2], collapse = ' '))
+refs[1]
+rownames(otus) <- refs
+otus <- tibble::rownames_to_column(otus, var = '#OTU ID')
+write.table(otus, file = 'data/stitched/como_stitched_otu_filtered_finalref.txt', sep = '\t', quote = F, row.names = F)
+
+
+
+
+
+
+
+
+
 
 
